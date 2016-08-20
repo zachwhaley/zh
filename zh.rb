517782ac71bd363803bdc5a2a1199b576f6aa045
Zachary Whaley <zachbwhaley@gmail.com>
2016-08-20 11:39:50 -0500

    Ruby: Add builtin commands 'cd' and 'exit'

--- zh.rb
+++ zh.rb
@@ -1,12 +1,27 @@
 #!/usr/bin/env ruby
 
+require 'shellwords'
+
+BUILTINS = {
+  'cd' => lambda { |dir| Dir.chdir(dir) },
+  'exit' => lambda { exit 0 },
+}
+
 loop do
   # Print prompt
   print 'ζ '
   # Get command
   cmd = $stdin.gets.strip
   next if cmd.empty?
-  # Fork and exec command
-  pid = fork { exec cmd }
-  Process.wait pid
+
+  # Parse command
+  prog, *args = Shellwords.shellsplit(cmd)
+  if BUILTINS[prog]
+    # Call builtin command
+    BUILTINS[prog].call(*args)
+  else
+    # Fork and exec command
+    pid = fork { exec(prog, *args)  }
+    Process.wait pid
+  end
 end
