93824f661022c8a877e646e3451a995ad26fb3b0
Zachary Whaley <zachbwhaley@gmail.com>
2016-08-21 09:22:39 -0500

    Go: Add builtin commands

--- zh.go
+++ zh.go
@@ -9,6 +9,17 @@ import (
 	"syscall"
 )
 
+var BUILTINS = map[string]func([]string){
+	"cd": func(args []string) {
+		if len(args) > 1 {
+			os.Chdir(args[1])
+		}
+	},
+	"exit": func(args []string) {
+		os.Exit(0)
+	},
+}
+
 func GetCmd() string {
 	// Scan users input
 	scan := bufio.NewScanner(os.Stdin)
@@ -16,16 +27,20 @@ func GetCmd() string {
 	return scan.Text()
 }
 
-func RunCmd(cmd string) {
+func RunCmd(args []string) {
+	// Check builtin commands
+	if run, ok := BUILTINS[args[0]]; ok {
+		// Run builtin command
+		run(args)
+		return
+	}
+
 	// List stdin, stdout, and stderr file descriptors
 	files := []uintptr{uintptr(syscall.Stdin), uintptr(syscall.Stdout), uintptr(syscall.Stderr)}
 	// Set process attributes
 	proc := syscall.ProcAttr{Files: files}
-	// Split command into slice of arguments
-	args := strings.Split(cmd, " ")
 	// Find the full path of the program
 	prog, _ := exec.LookPath(args[0])
-
 	// Fork and exec command
 	pid, _ := syscall.ForkExec(prog, args, &proc)
 	syscall.Wait4(pid, nil, 0, nil)
@@ -35,9 +50,15 @@ func main() {
 	for {
 		// Print prompt
 		fmt.Printf("ζ ")
+
 		// Get command
 		cmd := GetCmd()
-		// Run command
-		RunCmd(cmd)
+		// Parse command
+		args := strings.Split(cmd, " ")
+
+		if len(args) > 0 {
+			// Run command
+			RunCmd(args)
+		}
 	}
 }
